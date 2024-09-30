7d7341e1818acec8b291bcdeaa7d8e844a171e3c
jgonzales0221 <jgonz0221@gmail.com>
2024-09-29 23:49:43 -0600

    Final commit

--- meal_purchase.py
+++ meal_purchase.py
@@ -1,20 +1,20 @@
-# User input meal amount
-meal_price = float(input("Enter total amount of meal: "))
+def calculate_amounts():
+    # User input meal amount
+    meal_price = float(input("Enter total amount of meal: "))
+    
+    # Calculate the tip of the meal with 0.18 (18%) and round two decimal places
+    tip_amount = round((meal_price * 0.18), 2)
+    
+    # Calculate the sales tax of the meal with 0.07 (7%) and round two decimal places
+    sales_tax = round((meal_price * 0.07), 2)
+    
+    # Calculate the total amount by adding meal_price, tip_amount, and sales_tax
+    total_amount = meal_price + tip_amount + sales_tax
+    
+    # Print the results
+    print(f"Tip Amount: ${tip_amount:.2f}")
+    print(f"Sales Tax: ${sales_tax:.2f}")
+    print(f"Total Amount: ${total_amount:.2f}")
 
-# Calculate the tip of the meal with 0.18 (18%) and round two decimal places
-tip_amount = round((meal_price * 0.18), 2)
-
-# Print Tip amount with dollar sign in front
-print(f"Tip Amount: ${tip_amount:.2f}")
-
-# Calculate the sales tax of the meal with 0.07 (7%) and round two decimal places
-sales_tax = round((meal_price * 0.07), 2)
-
-# Print Sale Tax amount with dollar sign in front
-print(f"Sale Tax: ${sales_tax:.2f}")
-
-# Calulate total amount by adding meal_price, tip_amount, sales_ttax
-total_amount =(meal_price + tip_amount + sales_tax)
-
-# Print Total amount
-print(f"Total Amount: ${total_amount:.2f}")
+# Call the function to execute
+calculate_amounts()
